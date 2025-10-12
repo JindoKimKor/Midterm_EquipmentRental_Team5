2d6d02f6294f3a5de7a3681e0e518b48894995d7
JindoKimKor <jindokim.kor@gmail.com>
2025-10-12 19:16:20 -0400

    feat: add UnitOfWork implementation with empty structure
    - Create UnitOfWork class implementing IUnitOfWork
    - Add properties for Equipment, Customer, and Rental repositories
    - Add SaveChanges method stub for future implementation

---
+++ Midterm_EquipmentRental_Team5/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Midterm_EquipmentRental_Team5.Repositories.Interfaces;
+using Midterm_EquipmentRental_Team5.UnitOfWork.Interfaces;
+
+namespace Midterm_EquipmentRental_Team5.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IEquipmentRepository Equipment => throw new NotImplementedException();
+
+        public ICustomerRepository Customers => throw new NotImplementedException();
+
+        public IRentalRepository Rentals => throw new NotImplementedException();
+
+        public int SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
