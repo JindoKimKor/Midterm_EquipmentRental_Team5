42bd0895326ad4c61dd5c49d98bcbbd7059540db
JindoKimKor <jindokim.kor@gmail.com>
2025-10-12 19:05:16 -0400

    feat: add authentication DTOs and service interface
    - Create DTOs folder with LoginRequest and LoginResponse
    - Add IAuthService interface for authentication logic
    - Separate API communication (DTOs) from domain models (Customer)

---
+++ Midterm_EquipmentRental_Team5/Models/DTOs/LoginResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Midterm_EquipmentRental_Team5.Models.DTOs
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+    }
+}
---
+++ Midterm_EquipmentRental_Team5/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using Midterm_EquipmentRental_Team5.Models;
+
+namespace Midterm_EquipmentRental_Team5.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Customer? ValidateLogin(string username, string password);
+    }
+}
