764668c2904071822ffdf1027a1dae256dc188ab
NathanDinh <nathan.dinh03@outlook.com>
2025-10-17 18:29:08 -0400

    feat: Changed ClockSkew to 30 min

--- Midterm_EquipmentRental_Team5/Program.cs
+++ Midterm_EquipmentRental_Team5/Program.cs
@@ -55,7 +55,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidateIssuer = false,
             ValidateAudience = false,
             ValidateLifetime = true,
-            ClockSkew = TimeSpan.Zero
+            ClockSkew = TimeSpan.FromMinutes(30),
         };
     }
 );
