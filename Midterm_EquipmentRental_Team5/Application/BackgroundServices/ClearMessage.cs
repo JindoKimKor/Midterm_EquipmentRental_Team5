43490d38c3c6bace2bb2faef828723a397c26098
NathanDinh <nathan.dinh03@outlook.com>
2025-11-24 16:44:53 -0500

    fix: Fixed broken clear message worker

--- Midterm_EquipmentRental_Team5/Application/BackgroundServices/ClearMessage.cs
+++ Midterm_EquipmentRental_Team5/Application/BackgroundServices/ClearMessage.cs
@@ -1,20 +1,34 @@
-using Midterm_EquipmentRental_Team5.Infrastructure.Repositories.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+using Midterm_EquipmentRental_Team5.Application.Services.Interfaces;
+using Midterm_EquipmentRental_Team5.Presentation.Hubs;
 
 namespace Midterm_EquipmentRental_Team5.Application.BackgroundServices
 {
-    public class ClearMessage(IServiceScopeFactory serviceScopeFactory) : BackgroundService
+    public class ClearMessage(IHubContext<ChatHub> hubContext, IServiceScopeFactory serviceScopeFactory) : BackgroundService
     {
+        private readonly IHubContext<ChatHub> _hubContext = hubContext;
         private readonly IServiceScopeFactory _serviceScopeFactory = serviceScopeFactory;
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                using (var scope = _serviceScopeFactory.CreateScope())
+                try
                 {
-                    var chatRepository = scope.ServiceProvider.GetRequiredService<IChatRepository>();
-                    chatRepository.ClearAllMessages();
+                    // Create scope to access scoped IChatService
+                    using (var scope = _serviceScopeFactory.CreateScope())
+                    {
+                        var chatService = scope.ServiceProvider.GetRequiredService<IChatService>();
+                        chatService.ClearMessageFromDb();
+                    }
+
+                    await _hubContext.Clients.All.SendAsync("ClearAllMessages", cancellationToken: stoppingToken);
                 }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error in ClearMessage background service: {ex.Message}");
+                }
+
                 await Task.Delay(30000, stoppingToken);
             }
         }
